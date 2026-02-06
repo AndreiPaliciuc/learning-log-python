7f92696bdf57173b99a697b08e2994f318504b8c
Andrei Paliciuc <andrei.paliciuc26@gmail.com>
2026-02-07 00:53:02 +0100

    Lesson 03: Variables & Types (done)

---
+++ 01_basics/03_variables_types/main.py
@@ -0,0 +1,14 @@
+name = "Andrei"
+age = 24
+height = 1.74
+is_motivated = True
+
+print(name)
+print(age)
+print(height)
+print(is_motivated)
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_motivated))
