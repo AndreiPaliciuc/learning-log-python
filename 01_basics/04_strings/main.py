7b528068329f7dd391c1031420f69b0bde23f8c0
Andrei Paliciuc <andrei.paliciuc26@gmail.com>
2026-02-07 00:53:48 +0100

    Lesson 04: Strings (done)

---
+++ 01_basics/04_strings/main.py
@@ -0,0 +1,12 @@
+text = "Hello People"
+
+print(text)
+print(text.upper())
+print(text.lower())
+print(len(text))
+
+first_name = "Andrei"
+last_name = "Paliciuc"
+full_name = first_name + " " + last_name
+
+print(full_name)
